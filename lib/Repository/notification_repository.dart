f57f6adec78e623310775209472320ac99af3e1d
RavinduLakshan97 <ravindusachinthalakshan@gmail.com>
2020-02-18 11:50:45 +0530

    add notification repo class

---
+++ lib/Repository/notification_repository.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fcode_bloc/fcode_bloc.dart';
+import 'package:location_detecting_app/Model/notification.dart';
+import 'package:location_detecting_app/Util/db_util.dart';
+
+class NotificationRepository extends FirebaseRepository<Notification> {
+  @override
+  Notification fromSnapshot(DocumentSnapshot snapshot) {
+    if (snapshot == null) return null;
+    final data = snapshot.data;
+    if (data == null) return null;
+    return Notification(
+        ref: snapshot.reference,
+        title: data['title'] ?? "",
+        cordinate: data['cordinate'] ?? "");
+  }
+
+  @override
+  Stream<List<Notification>> query({SpecificationI specification, String type, DocumentReference parent}) {
+    return super.query(specification: specification, type: DBUtil.NOTIFICATION);
+  }
+
+  @override
+  Map<String, dynamic> toMap(Notification item) {
+    // TODO: implement toMap
+    return null;
+  }
+}
