ec8b93174bcd4428b75ac2dd8d1c41ed248a17c4
RavinduLakshan97 <ravindusachinthalakshan@gmail.com>
2020-02-27 14:13:24 +0530

    Add firebase live location updating

--- lib/UI/Map_Page/map_contain.dart
+++ lib/UI/Map_Page/map_contain.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location_detecting_app/Util/custom_colors.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapContainer extends StatefulWidget {
   @override
@@ -8,7 +10,48 @@ class MapContainer extends StatefulWidget {
 }
 
 class _mapContainPageState extends State<MapContainer> {
-  GoogleMapController _Controller;
+  bool mapToggle = false;
+
+  var currentLocation;
+
+  var clients = [];
+
+  GoogleMapController mapController;
+
+  void initState() {
+    super.initState();
+    Geolocator().getCurrentPosition().then((currloc) {
+      setState(() {
+        currentLocation = currloc;
+        mapToggle = true;
+        populateClients();
+      });
+    });
+  }
+
+  populateClients() {
+    clients = [];
+    Firestore.instance.collection('markers').getDocuments().then((docs) {
+      if (docs.documents.isNotEmpty) {
+        for (int i = 0; i < docs.documents.length; ++i) {
+          clients.add(docs.documents[i].data);
+          initMarker(docs.documents[i].data);
+        }
+      }
+    });
+  }
+
+  initMarker(clients){
+    mapController.clearMarkers().then((val) {
+      mapController.addMarker(
+        MarkerOptions(
+          position: LatLng(clients['location'].latitude , clients['location'].longitude),
+          draggable: false,
+          infoWindowText: InfoWindowText(clients['name'],""),
+        )
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,24 +66,30 @@ class _mapContainPageState extends State<MapContainer> {
       body: Stack(
         children: <Widget>[
           Container(
-            height: MediaQuery.of(context).size.height -50.0,
-            width: MediaQuery.of(context).size.width,
-            child: GoogleMap(
-              compassEnabled: true,
-              onMapCreated: mapCreated,
-              initialCameraPosition: CameraPosition(target: LatLng(7.8731, 80.7718),zoom: 7.0),
-             ),
-          ),
-        ],
+              height: MediaQuery.of(context).size.height - 50.0,
+              width: MediaQuery.of(context).size.width,
+              child: mapToggle
+                  ? GoogleMap(
+//                      compassEnabled: true,
+                      onMapCreated: onMapCreated,
+//                      initialCameraPosition: CameraPosition(
+//                          target: LatLng(currentLocation.latitude, currentLocation.longitude), zoom: 7.0),
 
+                    )
+                  : Center(
+                      child: Text(
+                        "Loading..Please wait...",
+                        style: TextStyle(fontSize: 20.0),
+                      ),
+                    )),
+        ],
       ),
     );
   }
 
-  void mapCreated(Controller){
+  void onMapCreated(Controller) {
     setState(() {
-      _Controller = Controller;
+      mapController = Controller;
     });
   }
-
 }
